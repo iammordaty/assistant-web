20674c5d857b73d6fc31c6a9ee2614675076c610
iammordaty <krzysztof.mortka@gmail.com>
2018-03-06 21:12:07 +0100

    Added experimental support for browsing filesystem and and running task from UI

---
+++ src/Assistant/Module/Common/Extension/Traits/GetTargetPath.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Assistant\Module\Common\Extension\Traits;
+
+use Assistant\Module\File\Extension\RecursiveDirectoryIterator;
+use Assistant\Module\File\Extension\PathFilterIterator;
+use Assistant\Module\File\Extension\IgnoredPathIterator;
+use Assistant\Module\File\Extension\SplFileInfo;
+use Assistant\Module\Track\Model\Track;
+use Assistant\Module\Directory\Model\Directory;
+
+trait GetTargetPath
+{
+    /**
+     * Zwraca ścieżkę katalogu, w którym powinien znaleźć się podany utwór
+     *
+     * @param Model $node
+     * @return string
+     */
+    private function getTargetPath($node)
+    {
+        // TODO: Do przemyślenia: getTargetPath musi przyjmować SplFileInfo, ponieważ funkcja move wymaga doceloweej nazwy (włącznie z katalogiem / plikiem przenoszonym)
+        // TODO: Uspójnić; możliwe, że trzeba będzie przenieść do odrębnej klasy
+
+        if ($node->isFile()) {
+            $subdir = strftime('%Y/%m. %B', $node->getMTime());
+
+            // TODO: "Other" powinno być w konfigu
+            return sprintf('/%s/%s/%s', 'Other', $subdir, $node->getBasename());
+        }
+
+        $iterator = new PathFilterIterator(
+            new RecursiveDirectoryIterator($node->getPathname(), RecursiveDirectoryIterator::SKIP_DOTS),
+            $node->getPathname(),
+            [ '@eaDir' ]
+        );
+
+        $iterator = new RecursiveDirectoryIterator($node->getPathname(), RecursiveDirectoryIterator::SKIP_DOTS);
+
+        $tracks = [];
+
+        // TODO: który iterator?
+        foreach (new \RecursiveIteratorIterator($iterator) as $track) {
+            $tracks[] = $track;
+        }
+
+    	if (empty($tracks)) {
+            return null;
+        }
+
+        usort($tracks, function($track1, $track2) {
+        	return $track1 === $track2 ? 0 : ($track1 > $track2 ? -1 : 1);
+        });
+
+        $subdir = strftime('%Y/%m. %B', $node->getMTime());
+
+        // TODO: "Singles" powinno być w konfigu
+        return sprintf('/%s/%s/%s', 'Singles', $subdir, $node->getBasename());
+    }
+}
