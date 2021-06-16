354fbb018a426cad78ea35148f1dcaa1a38f8e20
iammordaty <iammordaty@users.noreply.github.com>
2021-06-16 22:42:44 +0200

    Quick workaround for specific case in guid creation for directories

--- src/Assistant/Module/Common/Extension/SlugifyService.php
+++ src/Assistant/Module/Common/Extension/SlugifyService.php
@@ -21,8 +21,14 @@ final class SlugifyService implements SlugifyInterface
     /** Zwraca ścieżkę do katalogu, w której poszczególne poziomy są slugiem (url friendly) */
     public function slugifyPath(string $path): ?string
     {
+        // ręczne trimowanie nazwy katalogu oraz przekazanie trim = false jest obejściem
+        // tego że dla katalogów "/collection/other/2006" oraz "collection/other/- 2006"
+        // jest generowany ten sam slug, tj: "collection/other/2006";
+        // zastanowić się, czy można to obejść bardziej elegancko, systemowo i bez utraty na wydajności indeksowania
+        // (guid to także parent dla elementów potomnych)
+
         $dirs = array_map(
-            fn($dir) => $this->slugify->slugify($dir),
+            fn($dir) => $this->slugify->slugify(trim($dir), [ 'trim' => false ]),
             array_filter(explode(DIRECTORY_SEPARATOR, $path))
         );
 
