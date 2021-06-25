7629ab1479f98b14e5a2a0c9aee80f2492039d3b
iammordaty <iammordaty@users.noreply.github.com>
2021-06-25 16:31:15 +0200

    :lipstick: Simplified getUniqueGuid method in TrackWriter class

--- src/Assistant/Module/Collection/Extension/Writer/TrackWriter.php
+++ src/Assistant/Module/Collection/Extension/Writer/TrackWriter.php
@@ -50,26 +50,9 @@ final class TrackWriter implements WriterInterface
     /** Zwraca unikalny guid dla podanego utworu */
     private function getUniqueGuid(Track $track): string
     {
-        //<editor-fold desc="Wyjaśnienie dodatkowego warunku na guid">
-        /*
-            pierwszy warunek, pomimo tego że wydaje się że może być pokryty przez drugi, zabezpiecza guid
-            przed sytuacją w której przetwarzane są dwa pliki, z których jeden ma kolejny nr remiksu w nazwie,
-            a jest przetwarzany jako pierwszy. w takiej sytuacji nie ma miejsca dla "oryginalnego" guid-a, np.:
-            /collection/Singles/2009/08. sierpień/R.I.O/Shine On/R.I.O - 01 - Shine On [Original Mix].mp3
-            /collection/Singles/2009/08. sierpień/R.I.O/Shine On/R.I.O - 03 - Shine On [Radio Mix 2].mp3 [1]
-            /collection/Singles/2009/08. sierpień/R.I.O/Shine On/R.I.O - 10 - Shine On [Spencer & Hill Remix].mp3
-            /collection/Singles/2009/08. sierpień/R.I.O/Shine On/R.I.O - 02 - Shine On [Radio Mix].mp3 [2]
-            dla pliku [2] generowany jest guid z nr porządkowym ("r-i-o-shine-on-radio-mix-2"),
-            który jest już zajęty - w sposób prawidłowy - przez plik [1]
-        */
-        //</editor-fold>
+        $isGuidAvailable = ($this->trackService->findOneByGuid($track->getGuid()) === null);
 
-        $regex = Regex::exact($track->getGuid());
-        $searchCriteria = SearchCriteria::createFromGuid($regex);
-
-        $count = $this->trackService->count($searchCriteria);
-
-        if ($count === 0) {
+        if ($isGuidAvailable) {
             return $track->getGuid();
         }
 
