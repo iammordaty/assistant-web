38fbc665ebe176786a563027c978e1a1b0334f76
iammordaty <iammordaty@users.noreply.github.com>
2022-04-19 09:33:32 +0200

    :lipstick: Minor changes in track autocompleter

--- src/Assistant/Module/Collection/Extension/Autocomplete/TrackAutocompleteEntry.php
+++ src/Assistant/Module/Collection/Extension/Autocomplete/TrackAutocompleteEntry.php
@@ -5,18 +5,9 @@ namespace Assistant\Module\Collection\Extension\Autocomplete;
 final class TrackAutocompleteEntry
 {
     public function __construct(
-        private string $guid,
-        private string $name,
-        private string $url,
+        public readonly string $guid,
+        public readonly string $name,
+        public readonly string $url,
     ) {
-    }
-
-    public function toArray(): array
-    {
-        return [
-            'guid' => $this->guid,
-            'name' => $this->name,
-            'url' => $this->url,
-        ];
     }
 }
