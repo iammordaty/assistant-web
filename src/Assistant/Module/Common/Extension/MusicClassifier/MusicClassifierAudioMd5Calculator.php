d3baca37764f9e26bde8bd8a51c1e6a21b7dca39
iammordaty <iammordaty@users.noreply.github.com>
2022-12-28 21:51:18 +0100

    :elephant: :zap: :whale: Introduced MusicClassifierAudioMd5Calculator service

---
+++ src/Assistant/Module/Common/Extension/MusicClassifier/MusicClassifierAudioMd5Calculator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Assistant\Module\Common\Extension\MusicClassifier;
+
+use SplFileInfo;
+use Symfony\Component\Process\Process;
+
+final class MusicClassifierAudioMd5Calculator
+{
+    /**
+     * Uwaga, plik wykonywalny znajduje się w innym kontenerze.
+     *
+     * @see .docker/php-fpm/Dockerfile
+     * @see .docker/php-fpm/bin/essentia_streaming_md5
+     */
+    private const CALCULATOR_BINARY = 'essentia_streaming_md5';
+
+    public function calculate(SplFileInfo $track): string
+    {
+        $process = new Process([
+            self::CALCULATOR_BINARY,
+            $track->getPathname(),
+        ]);
+
+        $process
+            ->setIdleTimeout(null)
+            ->setTimeout(null)
+            ->run();
+
+        if (!$process->isSuccessful()) {
+            throw new MusicClassifierException($process);
+        }
+
+        // Przykładowy output:
+        // MD5 extractor computes MD5 value (...) metadata can differ.\n
+        // MD5: 7806cbe0450447d33d9e0eb570cd3069\n
+
+        $output = explode(PHP_EOL, $process->getOutput())[1];
+        $md5 = substr($output, 5);
+
+        return $md5;
+    }
+}
