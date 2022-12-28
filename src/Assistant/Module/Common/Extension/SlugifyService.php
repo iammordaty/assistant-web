ee224c90d01f33d01ccf20c764c82499eef93ce4
iammordaty <iammordaty@users.noreply.github.com>
2022-12-28 21:38:33 +0100

    :bug: Fixed minor bug in SlugifyService

--- src/Assistant/Module/Common/Extension/SlugifyService.php
+++ src/Assistant/Module/Common/Extension/SlugifyService.php
@@ -6,14 +6,11 @@ use Cocur\Slugify\Slugify;
 
 final class SlugifyService implements SlugifyInterface
 {
-    private Slugify $slugify;
-
-    public function __construct(?SlugifyInterface $slugify = null)
+    public function __construct(private Slugify $slugify = new Slugify())
     {
-        $this->slugify = $slugify ?: new Slugify();
     }
 
-    public function slugify(string $string, array $options = null): ?string
+    public function slugify(string $string, ?array $options = null): ?string
     {
         return $this->slugify->slugify($string, $options);
     }
@@ -21,15 +18,23 @@ final class SlugifyService implements SlugifyInterface
     /** Zwraca ścieżkę do katalogu, w której poszczególne poziomy są slugiem (url friendly) */
     public function slugifyPath(string $path): ?string
     {
-        // ręczne trimowanie nazwy katalogu oraz przekazanie trim = false jest obejściem
-        // tego że dla katalogów "/collection/other/2006" oraz "collection/other/- 2006"
+        // ręczne trim-owanie nazwy katalogu oraz przekazanie trim = false jest obejściem
+        // tego, że dla katalogów "/collection/other/2006" oraz "collection/other/- 2006"
         // jest generowany ten sam slug, tj: "collection/other/2006";
         // zastanowić się, czy można to obejść bardziej elegancko, systemowo i bez utraty na wydajności indeksowania
         // (guid to także parent dla elementów potomnych)
+        // jednocześnie rtrim zapobiega dodawaniu "-" na końcu utworów z remiksem w nawiasach w ścieżkach, np.:
+        // "/.../Just Like You (Joris Voorn Remix)" -> "/.../just-like-you-joris-voorn-remix-"
 
         $dirs = array_map(
-            fn($dir) => $this->slugify->slugify(trim($dir), [ 'trim' => false ]),
-            array_filter(explode(DIRECTORY_SEPARATOR, $path))
+            function (string $dir): string {
+                $dir = trim($dir);
+                $dir = $this->slugify->slugify($dir, [ 'trim' => false ]);
+                $dir = rtrim($dir, '-');
+
+                return $dir;
+            },
+            array_merge(array_filter(explode(DIRECTORY_SEPARATOR, $path)))
         );
 
         return implode(DIRECTORY_SEPARATOR, $dirs) ?: null;
