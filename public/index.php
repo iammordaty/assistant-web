b025fe188df5490fe2e62b43d855ea5a89af1a11
iammordaty <iammordaty@users.noreply.github.com>
2023-08-16 10:56:06 +0200

    :recycle: Replaced a BASE_DIR constant in favor of a variable

--- public/index.php
+++ public/index.php
@@ -3,28 +3,29 @@
 use DI\Bridge\Slim\Bridge;
 use DI\ContainerBuilder;
 
+/* @noinspection DuplicatedCode */
 setlocale(LC_TIME, 'pl_PL.utf8');
 
-define('BASE_DIR', dirname(__DIR__));
+$baseDir = dirname(__DIR__);
 
 if (function_exists('xdebug_set_filter')) {
-    xdebug_set_filter(XDEBUG_FILTER_STACK, XDEBUG_PATH_EXCLUDE, [ BASE_DIR . '/vendor/' ]);
+    xdebug_set_filter(XDEBUG_FILTER_STACK, XDEBUG_PATH_EXCLUDE, [ $baseDir . '/vendor/' ]);
 }
 
-require_once BASE_DIR . '/vendor/autoload.php';
+require_once $baseDir . '/vendor/autoload.php';
 
-$config = (require_once BASE_DIR . '/config/config.inc')(BASE_DIR);
+$config = (require_once $baseDir . '/config/config.inc')($baseDir);
 
 /** @noinspection PhpUnhandledExceptionInspection */
 $container = (new ContainerBuilder())
-    ->addDefinitions((require_once BASE_DIR . '/config/container.inc')(BASE_DIR, $config))
+    ->addDefinitions((require_once $baseDir . '/config/container.inc')($baseDir, $config))
     ->build();
 
 $app = Bridge::create($container);
 
-(require_once BASE_DIR . '/config/routes.inc')($app);
-(require_once BASE_DIR . '/config/middleware.inc')($app);
+(require_once $baseDir . '/config/routes.inc')($baseDir, $app);
+(require_once $baseDir . '/config/middleware.inc')($app);
 
-unset($config, $container);
+unset($baseDir, $config, $container);
 
 $app->run();
