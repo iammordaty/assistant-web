6e3d43398cb60244589a72b597cb0acc8793ab37
iammordaty <iammordaty@users.noreply.github.com>
2023-08-16 10:56:06 +0200

    :recycle: Replaced a BASE_DIR constant in favor of a variable

--- bin/console.php
+++ bin/console.php
@@ -3,21 +3,22 @@
 use DI\ContainerBuilder;
 use Symfony\Component\Console\Application;
 
+/* @noinspection DuplicatedCode */
 setlocale(LC_TIME, 'pl_PL.utf8');
 
-define('BASE_DIR', dirname(__DIR__));
+$baseDir = dirname(__DIR__);
 
 if (function_exists('xdebug_set_filter')) {
-    xdebug_set_filter(XDEBUG_FILTER_STACK, XDEBUG_PATH_EXCLUDE, [ BASE_DIR . '/vendor/' ]);
+    xdebug_set_filter(XDEBUG_FILTER_STACK, XDEBUG_PATH_EXCLUDE, [ $baseDir . '/vendor/' ]);
 }
 
-require_once BASE_DIR . '/vendor/autoload.php';
+require_once $baseDir . '/vendor/autoload.php';
 
-$config = (require_once BASE_DIR . '/config/config.inc')(BASE_DIR);
+$config = (require_once $baseDir . '/config/config.inc')($baseDir);
 
 /** @noinspection PhpUnhandledExceptionInspection */
 $container = (new ContainerBuilder())
-    ->addDefinitions((require_once BASE_DIR . '/config/container.inc')(BASE_DIR, $config))
+    ->addDefinitions((require_once $baseDir . '/config/container.inc')($baseDir, $config))
     ->build();
 
 set_error_handler(function (int $type, string $msg, string $file, int $line) {
@@ -30,9 +31,9 @@ set_error_handler(function (int $type, string $msg, string $file, int $line) {
 });
 
 $console = new Application();
-$console->addCommands((require_once BASE_DIR . '/config/tasks.inc')($container));
+$console->addCommands((require_once $baseDir . '/config/tasks.inc')($container));
 
-unset($container);
+unset($baseDir, $container);
 
 /** @noinspection PhpUnhandledExceptionInspection */
 $console->run();
