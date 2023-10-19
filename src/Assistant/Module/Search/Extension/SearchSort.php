9d12ebe90513003083d437fd7d8f92a0db6260c4
iammordaty <iammordaty@users.noreply.github.com>
2023-10-19 21:10:48 +0200

    :sparkles: Added the ability to search by best match in the Search module

--- src/Assistant/Module/Search/Extension/SearchSort.php
+++ src/Assistant/Module/Search/Extension/SearchSort.php
@@ -6,18 +6,20 @@ use Assistant\Module\Common\Storage\Storage;
 
 final class SearchSort
 {
-    // Tutaj należałoby jeszcze uwzględnić najlepsze dopasowanie,
-    // czyli to, po czym domyślnie sortuje wyszukiwarka prosta.
-    private const ARTIST = 'a';
-    private const ARTIST_DESCENDING = 'ad';
-    private const NEWEST = 'yd';
-    private const OLDEST = 'y';
-    private const RECENTLY_INDEXED = 'id';
-    private const LATEST_INDEXED = 'i';
+    public const ARTIST = 'a';
+    public const ARTIST_DESCENDING = 'ad';
+    public const NEWEST = 'yd';
+    public const OLDEST = 'y';
+    public const RECENTLY_INDEXED = 'id';
+    public const LATEST_INDEXED = 'i';
+    public const TEXT_SCORE = 't';
 
-    public static function create(?string $sort): array
+    public static function create(?string $sort, string $default): array
     {
+        $sort = $sort ?: $default;
+
         $sort = match ($sort) {
+            self::ARTIST => [ 'guid' => Storage::SORT_ASC ],
             self::ARTIST_DESCENDING => [ 'guid' => Storage::SORT_DESC ],
 
             self::NEWEST => [ 'year' => Storage::SORT_DESC ],
@@ -26,7 +28,7 @@ final class SearchSort
             self::RECENTLY_INDEXED => [ 'indexed_date' => Storage::SORT_DESC ],
             self::LATEST_INDEXED => [ 'indexed_date' => Storage::SORT_ASC ],
 
-            default => [ 'guid' => Storage::SORT_ASC ],
+            self::TEXT_SCORE => Storage::SORT_TEXT_SCORE_DESC,
         };
 
         return $sort;
