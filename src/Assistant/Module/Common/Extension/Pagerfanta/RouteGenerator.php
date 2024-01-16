31fe7111fe256a1e68af1bc82ad43de9fd9e0289
iammordaty <iammordaty@users.noreply.github.com>
2024-01-16 22:08:59 +0100

    :bug: Minor pagination fixes

--- src/Assistant/Module/Common/Extension/Pagerfanta/RouteGenerator.php
+++ src/Assistant/Module/Common/Extension/Pagerfanta/RouteGenerator.php
@@ -6,23 +6,36 @@ use Assistant\Module\Common\Extension\Route;
 use Assistant\Module\Common\Extension\RouteResolver;
 use Pagerfanta\RouteGenerator\RouteGeneratorInterface;
 
-final class RouteGenerator implements RouteGeneratorInterface
+final readonly class RouteGenerator implements RouteGeneratorInterface
 {
     public function __construct(
-        private readonly RouteResolver $routeResolver,
-        private readonly ?Route $route
+        private RouteResolver $routeResolver,
+        private ?Route $route,
     ) {
     }
 
     public function __invoke(int $page): string
     {
-        $baseUrl = $this->route ? $this->routeResolver->resolve($this->route) : '';
+        $baseUrl = '';
+        $route = $this->route;
+
+        if ($route) {
+            $query = $route->getQuery();
+
+            if (isset($query['page'])) {
+                unset($query['page']);
+
+                $route = $route->withQuery($query);
+            }
+
+            $baseUrl = $this->routeResolver->resolve($route);
+        }
 
         if ($page === 1) {
             return $baseUrl;
         }
 
-        $urlWithPage = $this->route->getQuery()
+        $urlWithPage = $route->getQuery()
             ? sprintf('%s&page=%d', $baseUrl, $page)
             : sprintf('?page=%d', $page);
 
